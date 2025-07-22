142cdf63d1571317b22d91f87fe874cc05f0416b
noarama <noarama@uvic.ca>
2025-07-22 13:02:04 -0700

    Removing unused functions

--- theory.py
+++ theory.py
@@ -57,69 +57,6 @@ def case_one(ccw_x, cw_x, p):
     return ccw_prob, cw_prob
 
 
-
-# @lru_cache(maxsize=None)
-# def ladder_connectivity(x,p):
-#     if x == 0:
-#         return p
-
-#     return p*((p**x) + ladder_connectivity(x-1, p) - (p**x) * theta(p, x, 0))
-
-# @lru_cache(maxsize=None)
-# def theta(p,x, flag):
-#     ''' This function calculates theta which is part of the theoretical equations. 
-#     This function uses:
-#     p - The bond probability, 
-#     x - the current size, 
-#     flag - a flag to indicate which case we are in, 
-#     small - a variable that holds a smaller case. This is used to make the recursion less deep.
-#     small_x - The size of the small case we already have. 
-
-#     For clarity, we are trying to use previously found cases to make the recursion less deep by using previously 
-#     obtained cases. If we do not have a case already obtained, the value of small and small_x is set to -1. If we do have
-#     a previously obtained case, small will hold its value and small_x will hold its size. 
-#     '''
-#     # These are the base cases, depending on the case we are handeling
-#     if flag == 2 and x == 1:
-#         return 0
-
-#     if flag == 4 and x == 1:
-#         return p
-
-#     if flag == 0 and x == 0:
-#         return 0
-
-#     # # This if block is to shorten the running time
-#     # if small != -1 and x == small_x:
-#     #     return small
-
-#     # This is the recursion itself 
-#     return (p* (p + (1-p)*theta(p,x-1, flag)))
-
-
-
-# def case_one(CCW_x, CW_x, p):
-#     ''' This function is responsible for calculating the probability of the two ladders under case one: S and D disconnected.
-#     This function is recursive and it uses:
-#     CCW_x - The size of the counter-clockwise direction ladder,
-#     CW_x - the size of the clockwise direction ladder,
-#     p - the bond probability.
-#     '''
-
-#     CCW = 0 
-#     CW = 0
-
-#     if CCW_x > 1:
-#         CCW = (p**2) * ladder_connectivity(CCW_x - 2, p)
-    
-#     if CW_x > 1:
-#         CW = (p**2) * ladder_connectivity(CW_x - 2, p)
-
-
-#     return CCW, CW 
-
-
-
 def case_two_three(CCW_x, CW_x, p, CCW_one, CW_one):
     ''' This function is responsible for calculating the probability of the two ladders under case two and three: one of S and D is disconnected. 
     Thos function is uses:
